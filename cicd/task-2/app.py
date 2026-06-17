acae2b96f890dd98efd2e12eff6a0844721b0b24
AbdimajidHussein03 <Abdimajid819@gmail.com>
2026-06-17 17:09:02 +0100

    update task 2 to flask docker app

--- cicd/task-2/app.py
+++ cicd/task-2/app.py
@@ -1,1 +1,21 @@
-print("Hello from Task 2, This is a Docker CD workflow :))")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return """
+    <h1>Task 2 Docker CD App</h1>
+    <p>This Flask web app is running inside a Docker container.</p>
+    <p>The Docker image was built and pushed using GitHub Actions.</p>
+    """
+
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
